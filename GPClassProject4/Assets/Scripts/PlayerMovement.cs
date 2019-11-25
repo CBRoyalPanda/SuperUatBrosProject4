ca51a9d934b94612b8ecb267dd9dc8fb855d1035
CBRoyalPanda <50720170+CBRoyalPanda@users.noreply.github.com>
2019-11-24 23:28:52 -0700

    The Whole Thing
    Everything

---
+++ GPClassProject4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float groundSPD = 5;
+
+    public bool OnGround;
+    public int DoubleJumps = 1;
+    public int MaxDoubleJumps = 1;
+
+    public LayerMask groundLayers;
+
+    private SpriteRenderer mySpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Is the player grounded
+        OnGround = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.3f, transform.position.y - 0.3f),
+            new Vector2(transform.position.x + 0.3f, transform.position.y - 0.31f), groundLayers);
+
+       
+        
+        //When the button is pressed, a jump is activated
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (OnGround == true)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                DoubleJumps = MaxDoubleJumps;
+            }
+            else if (DoubleJumps > 0)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                DoubleJumps -= DoubleJumps;
+            }
+            else
+            {
+
+            }
+
+        }
+
+        //When the D key is pressed the player faces right
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            mySpriteRenderer.flipX = false;
+        }
+
+        //When the D key is pressed the player faces left
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            mySpriteRenderer.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //When the button is held the player moves to the right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * groundSPD * Time.deltaTime);
+        }
+
+
+        //When the D key is pressed the player faces left
+        //When the button is held the player moves to the left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * groundSPD * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(3);
+
+        }
+
+        
+    }
+   
+}
---
+++ GPClassProject4/Assets/Scripts/PlayerRun.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : StateMachineBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerRigidbody = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
+        {
+
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+        }
+
+        if (playerRigidbody.velocity.y < -0.1)
+        {
+            animator.SetBool("IsFalling", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
